155acdf6b4f7e9854c8025abbebab8af1274fdd6
maze0417 <maze0417@gmail.com>
2016-03-08 14:17:32 +0800

    refactor

---
+++ Projects/05 Business Logic Layer/EME.Data.Services/Factory/UnityFactory.cs
@@ -0,0 +1,25 @@
+﻿using EME.Application.Interfaces;
+using EME.Application.Services;
+using EME.Data.SqlRepositories;
+using EME.Infrastructure.Common.Configurations;
+using Microsoft.Practices.Unity;
+
+namespace EME.Application.Factory
+{
+    public static class UnityFactory
+    {
+        private static LifetimeManager NewEachTime => new PerResolveLifetimeManager();
+        private static LifetimeManager ReuseWithinContainer => new ContainerControlledLifetimeManager();
+
+        public static IUnityContainer RegisterComponents()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IUserService, UserService>(NewEachTime);
+            container.RegisterType<IDbRepository, DbRepository>(NewEachTime);
+            container.RegisterType<IDataConfig, DataConfig>(ReuseWithinContainer);
+
+            return container;
+        }
+    }
+}
---
+++ Projects/05 Business Logic Layer/EME.Data.Services/Mappers/UserMapper.cs
@@ -0,0 +1,24 @@
+﻿using EME.Application.Responses.UserService;
+using EME.Data.Models.DbEntities;
+
+namespace EME.Application.Mappers
+{
+    public class UserMapper
+    {
+        public GetUserResponse Response(User user)
+        {
+            return new GetUserResponse
+            {
+                Email = user.Email,
+                Firstname = user.Firstname,
+                Id = user.Id,
+                Lastname = user.Lastname,
+                Password = user.Password,
+                Role = user.Role,
+                Salt = user.Salt,
+                Status = user.Status,
+                Username = user.Username
+            };
+        }
+    }
+}
